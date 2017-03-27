02e5d6d648d695a1e5065b8679c8460b8028e11f
Jaime Echeverría <rpggrenade@gmail.com>
2017-03-26 23:55:16 -0300

    Tarea 2

---
+++ app/controllers/courses_controller.rb
@@ -0,0 +1,96 @@
+class CoursesController < ApplicationController
+  before_action :set_course, only: [:show, :edit, :update, :destroy]
+
+  # GET /courses
+  # GET /courses.json
+  def index
+    @courses = Course.all
+  end
+
+  # GET /courses/1
+  # GET /courses/1.json
+  def show
+  end
+
+  # GET /courses/new
+  def new
+    @course = Course.new
+  end
+
+  # GET /courses/1/edit
+  def edit
+  end
+
+  # POST /courses
+  # POST /courses.json
+  def create
+    @course = Course.new(course_params)
+
+    respond_to do |format|
+      if @course.save
+        format.html { redirect_to @course, notice: 'Course was successfully created.' }
+        format.json { render :show, status: :created, location: @course }
+      else
+        format.html { render :new }
+        format.json { render json: @course.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # PATCH/PUT /courses/1
+  # PATCH/PUT /courses/1.json
+  def update
+    respond_to do |format|
+      if @course.update(course_params)
+        format.html { redirect_to @course, notice: 'Course was successfully updated.' }
+        format.json { render :show, status: :ok, location: @course }
+      else
+        format.html { render :edit }
+        format.json { render json: @course.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def add_students
+    @course = Course.find(params[:course_id])
+    actual_students_on_class = CourseStudent.where(course_id: params[:course_id])
+
+    if params[:student_id]
+      CourseStudent.create(course_id: params[:course_id],  person_id: params[:student_id])
+    end
+
+    students_a_mostrar =  Array.new
+    for student in Person.all
+      agrega = 1
+      for student_b in actual_students_on_class
+        if student.id == student_b.person_id
+          agrega = 0
+        end
+      end
+      if agrega == 1
+        students_a_mostrar << student
+      end
+    end
+    @students = students_a_mostrar
+  end
+  # DELETE /courses/1
+  # DELETE /courses/1.json
+  def destroy
+    @course.destroy
+    respond_to do |format|
+      format.html { redirect_to courses_url, notice: 'Course was successfully destroyed.' }
+      format.json { head :no_content }
+    end
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_course
+      @course = Course.find(params[:id])
+    end
+
+    # Never trust parameters from the scary internet, only allow the white list through.
+    def course_params
+      params.require(:course).permit(:title, :code, :person_id)
+    end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,14 @@
 Rails.application.routes.draw do
+  resources :course_students, :courses, :people
+  root 'courses#index'
+
+  get 'people' => 'people#index'
+  get 'course_students' => 'course_students#index'
+
+  resource :courses do
+    get 'add_students', :on => :collection
+  end
+  #map.connect '/course/add_students', :controller => 'courses', :action => 'add_students'
+  #root 'people#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
